64c6ee102cf62a0ab4e76c3bf903fc2ec1436374
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2021-10-23 19:01:35 -0400

    add button variants

--- src/components/generic/Button.js
+++ src/components/generic/Button.js
@@ -2,65 +2,65 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
-const Wrapper = styled.div`
-  border: 1px solid #121212;
-  margin: 20px 0px;
+const BaseButton = styled.button`
+    color: #ffffff;
+    border: none;
+    padding: 10px 20px;
+    font-size: 30px;
+    font-weight: 700;
 `;
 
-const btnVariantToBgColor = (btnVariant) => {
-    
-    const bgColorMap = {
-        primary: { 
-            standard: "blue", 
-            hover: "dark blue",
-            active: "purple",
-            disabled: "grey"
-        },
-        secondary: { 
-            standard: "", 
-            hover: "dark blue",
-            active: "purple",
-            disabled: "grey"
-        },
-        success: { 
-            standard: "blue", 
-            hover: "dark blue",
-            active: "purple",
-            disabled: "grey"
-        },
-        danger: { 
-            standard: "red", 
-            hover: "pink",
-            active: "burgundy",
-            disabled: "grey"
-        },
-        warning: { 
-            standard: "blue", 
-            hover: "dark blue",
-            active: "purple",
-            disabled: "grey"
-        }
+const ButtonPrimary = styled(BaseButton)`
+    background-color: #45D3AD;
+
+    :hover {
+        background-color: #419786;
+    }
+`;
+
+const ButtonSecondary = styled(BaseButton)`
+    background-color: #E16BBD;
+
+    :hover {
+        background-color: #B64995;
+    }
+`;
+
+const ButtonDanger = styled(BaseButton)`
+    background-color: #D34545;
+
+    :hover {
+        background-color: #A42323;
     }
 
-    const textColorMap = {
-
-    }
-
-}
+`;
 
 class Button extends React.Component {
-  render() {
-    const { type, onClick, disabled, variant, children } = this.props;
-    return (
-        <button {...{ type, onClick, disabled }}>{children}</button>
-    );
-  }
+    render() {
+        const { children, variant, ...passProps } = this.props;
+
+        switch(variant) {
+            case "secondary":
+                return (
+                    <ButtonSecondary {...{ passProps }}>{children}</ButtonSecondary>
+                );
+            case "danger": 
+                return (
+                    <ButtonDanger {...{ passProps }}>{children}</ButtonDanger>
+                );
+            case "primary":
+            default: 
+                return (
+                    <ButtonPrimary {...{ passProps }}>{children}</ButtonPrimary>
+                );
+        }
+    }
 }
 Button.propTypes = {
     type: PropTypes.oneOf(["button", "submit", "reset"]),
     onClick: PropTypes.func.isRequired,
     disabled: PropTypes.bool,
-    variant: PropTypes.oneOf(["primary", "secondary", "success", "danger", "warning"]),
+    variant: PropTypes.oneOf(["primary", "secondary", "danger"]),
     children: PropTypes.oneOfType([
         PropTypes.arrayOf(PropTypes.node),
         PropTypes.node
