df5a34488189f559f55c85d44331e5a261d6567a
Bianca Morris <Bianca_Morris@hms.harvard.edu>
2021-10-23 18:16:37 -0400

    start timer pane styling

---
+++ src/components/generic/Panel.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const ImageBackgroundPane = styled.div`
+    background: linear-gradient(120deg,rgba(143,229,255,0.9),rgba(0,20,168,0.78)),url("/maarten-van-den-heuvel-unsplash-sm.jpg");
+    height: 390px;
+    width: 550px;
+    border-radius: 0 20px 20px 0;
+    padding: 15px 20px 5px 20px;
+    background-position: center;
+    background-size: cover;
+`;
+
+const InnerPane = styled.div`
+    box-sizing: border-box;
+    flex-shrink: 0;
+    border-radius: 20px;
+    border: 4px dotted #FFFFFF;
+    height: 380px;
+    width: 100%;
+`;
+
+
+class Panel extends React.Component {
+
+    render() {
+        const { children } = this.props;
+        return (
+            <ImageBackgroundPane>
+                <InnerPane>
+                { children }
+                </InnerPane>  
+            </ImageBackgroundPane>
+        );
+    }
+    
+}
+
+export default Panel;
--- src/views/TimersView.js
+++ src/views/TimersView.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
+import Panel from "../components/generic/Panel";
 import Stopwatch from "../components/timers/Stopwatch";
 import Countdown from "../components/timers/Countdown";
 import XY from "../components/timers/XY";
@@ -10,18 +11,20 @@ const Timers = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  justify-content: center;
 `;
 
 const TimerSelector = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  width: 433px;
-  height: 588px;
+  width: 250px;
+  height: 350px;
   justify-content: center;
   background: linear-gradient(143deg, #617cb7 0%, rgb(35, 48, 77) 100%);
   overflow: visible;
   border-radius: 20px 0 0 20px;
+  padding: 30px;
 `;
 
 const Timer = styled.div`
@@ -31,7 +34,24 @@ const Timer = styled.div`
   font-size: 1.5rem;
 `;
 
-const TimerTitle = styled.div``;
+const TimerTitle = styled.div`
+  background-color: #2b3254;
+  color: #459fd3;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 20px 0;
+  margin-top: 10px;
+  margin-bottom: 10px;
+  text-transform: uppercase;
+  font-weight: 700;
+  cursor: pointer;
+`;
+
+const ActiveTimerTitle = styled(TimerTitle)`
+  background-color: #61cbf5;
+  color: #ffffff;
+`;
 
 function App() {
   const timers = [
@@ -46,11 +66,16 @@ function App() {
   return (
     <Timers>
       <TimerSelector >
-      {timers.map((timer, idx) => (
-        <TimerTitle key={timer.title} onClick={() => setTimerIdx(idx)}>{timer.title}</TimerTitle>
-      ))}
+      {timers.map((timer, idx) => {
+        if (timerIdx === idx) {
+          return <ActiveTimerTitle key={timer.title} onClick={() => setTimerIdx(idx)}>{timer.title}</ActiveTimerTitle>;
+        }
+        return <TimerTitle key={timer.title} onClick={() => setTimerIdx(idx)}>{timer.title}</TimerTitle>;
+      })}
       </TimerSelector>
-      {timers[timerIdx].C}
+      <Panel>
+        {timers[timerIdx].C}
+      </Panel>
     </Timers>
   );
 }
